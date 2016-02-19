24cfdd294bdd482af0e2c7fef40af0f123c8e4fd
Milan Arandjelovic <milanarandjelovic90@gmail.com>
2016-02-18 23:36:48 -0800

    Finish login/logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,31 @@
 #
 
 class UsersController < ApplicationController
-  def new
-  end
+
+	def show
+        @user = User.find(params[:id])
+	end
+
+    def new
+        @user = User.new
+    end
+
+	def create
+    @user = User.new(users_params)
+        if @user.save
+            # Save user
+            log_in @user
+            flash[:success] = 'REGISTER'
+            redirect_to @user
+        else
+	        render 'new'
+	    end
+	end
+
+	# Define strong params
+	private
+        # Define private users_params
+        def users_params
+            params.require(:user).permit(:name, :email, :password, :password_confirmation)
+        end
 end
